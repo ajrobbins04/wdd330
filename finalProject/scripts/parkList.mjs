653d9a399b339e00e012035b6d8b41e5cb1e2645
ajrobbins04 <ajrobbins04@gmail.com>
2023-06-24 16:14:39 -0700

    about to attempt hoverlink highlighting

--- finalProject/scripts/parkList.mjs
+++ finalProject/scripts/parkList.mjs
@@ -12,38 +12,9 @@ export default async function parkList(selector) {
     console.log(parks);
 }
 
-// returns array of park names from A - Z
-/*function getNameList(parks) {
+function sortByState(data) {
 
-    let names = [];
-    for (let i = 0; i < parks.limit; i++) {
-        names.push(parks.data[i].fullName);
-    }
-    return names;
 }
-
-// returns array of park images
-function getImagesList(parks) {
-    let images = [];
-    for (let i = 0; i < parks.limit; i++) {
-        images.push(parks.data[i].images[0]);
-    }
-    return images;
-}
-
-function getLocation(parks) {
-
-}*/
-
-/*function parkResultTemplate(parks) {
-    return `<li class="park-result">
-    <div class="park-result-img">
-        <img src="${parks.data.images[0].url}" alt="${parks.data.images[0].altText}">
-    </div>
-    <p class="park-result-name">${parks.data.fullName}</p>
-    <p class="park-result-state">${parks.data.states}</p>`;
-}*/
-
 
 function parkResultTemplate(data) {
  
@@ -55,11 +26,15 @@ function parkResultTemplate(data) {
     // it in the html
     if (data.images.length > 0) {
         return `<li class="parkResult">
-        <h2 class="name parkResult-name">${data.fullName}</h2>
+        <a href=parkDetails.html">
+            <h2 class="name parkResult-name">${data.fullName}</h2>
+        </a>
         <div class="hover overlay">
-            <picture class="parkResult-img">
-                <img src="${data.images[0].url}" alt="${data.images[0].altText}">
-            </picture>
+            <a href="parkDetails.html">
+                <picture class="parkResult-img">
+                    <img src="${data.images[0].url}" alt="${data.images[0].altText}">
+                </picture>
+            </a>
             <div class="overlay-description">
                 <p class="description parkResult-description">${data.description}</p>
             </div>
@@ -71,7 +46,9 @@ function parkResultTemplate(data) {
     // for parks without an image
     else {
         return `<li class="parkResult">
-        <h2 class="name parkResult-name">${data.fullName}</h2>
+        <a href=parkDetails.html">
+            <h2 class="name parkResult-name">${data.fullName}</h2>
+        </a>
         <p class="parkResult-noImg">[No Image Provided]</p>
         <p class="location parkResult-state">Location: ${fullStates}</p>
         <p class="description parkResult-description">${data.description}</p>
