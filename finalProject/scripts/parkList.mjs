7efd30576abe85a2da2a1f0ccd5676e0aca8829d
ajrobbins04 <ajrobbins04@gmail.com>
2023-06-26 15:50:42 -0700

    formatting changes

--- finalProject/scripts/parkList.mjs
+++ finalProject/scripts/parkList.mjs
@@ -43,32 +43,28 @@ function parkResultTemplate(data) {
     // it in the html
     if (data.images.length > 0) {
         return `<li class="parkResult">
-        <a href=parkDetails.html">
-            <h2 class="name parkResult-name">${data.fullName}</h2>
-        </a>
+        <h2 class="name parkResult-name">${data.fullName}</h2>
+        <p class="location parkResult-location"><em>Located in ${fullStates}</em></p>
         <div class="hover overlay">
-            <a href="parkDetails.html">
-                <picture>
-                    <img class="parkResult-img" src="${data.images[0].url}" alt="${data.images[0].altText}">
-                </picture>
-            </a>
+           <picture>
+                <img class="parkResult-img" src="${data.images[0].url}" alt="${data.images[0].altText}">
+            </picture>
             <div class="overlay-description">
                 <p class="description parkResult-description">${data.description}</p>
             </div>
         </div>
-        <p class="location parkResult-location">Location: ${fullStates}</p>
+        <a href="parkDetails.html">Learn More</a>
         </li>`;
     }
 
     // for parks without an image
     else {
         return `<li class="parkResult">
-        <a href=parkDetails.html">
-            <h2 class="name parkResult-name">${data.fullName}</h2>
-        </a>
+        <h2 class="name parkResult-name">${data.fullName}</h2>
+        <p class="location parkResult-location"><em>Located in ${fullStates}</em></p>
         <p class="parkResult-noImg">[No Image Provided]</p>
-        <p class="location parkResult-state">Location: ${fullStates}</p>
         <p class="description parkResult-description">${data.description}</p>
+        <a href="parkDetails.html">Learn More</a>
         </li>`;
     }
 }
