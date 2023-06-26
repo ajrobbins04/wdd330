b5e369273b3e4194441af83704801fa3b0868016
ajrobbins04 <ajrobbins04@gmail.com>
2023-06-26 16:14:18 -0700

    changes

--- finalProject/scripts/parkList.mjs
+++ finalProject/scripts/parkList.mjs
@@ -44,7 +44,7 @@ function parkResultTemplate(data) {
     if (data.images.length > 0) {
         return `<li class="parkResult">
         <h2 class="name parkResult-name">${data.fullName}</h2>
-        <p class="location parkResult-location"><em>Located in ${fullStates}</em></p>
+        <p class="location parkResult-location">Located in ${fullStates}</p>
         <div class="hover overlay">
            <picture>
                 <img class="parkResult-img" src="${data.images[0].url}" alt="${data.images[0].altText}">
@@ -53,7 +53,7 @@ function parkResultTemplate(data) {
                 <p class="description parkResult-description">${data.description}</p>
             </div>
         </div>
-        <a href="parkDetails.html">Learn More</a>
+        <a class="parkResult-learnMore" href="parkDetails.html">Learn More</a>
         </li>`;
     }
 
@@ -61,10 +61,10 @@ function parkResultTemplate(data) {
     else {
         return `<li class="parkResult">
         <h2 class="name parkResult-name">${data.fullName}</h2>
-        <p class="location parkResult-location"><em>Located in ${fullStates}</em></p>
+        <p class="location parkResult-location">Located in ${fullStates}</p>
         <p class="parkResult-noImg">[No Image Provided]</p>
         <p class="description parkResult-description">${data.description}</p>
-        <a href="parkDetails.html">Learn More</a>
+        <a class="parkResult-learnMore" href="parkDetails.html">Learn More</a>
         </li>`;
     }
 }
